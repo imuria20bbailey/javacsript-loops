9bcb0c526725442737451d43b2906807d74d9e73
imuria20bbailey <barbraoluka20@gmail.com>
2022-10-20 23:50:59 +0300

    final work for assignment 2

--- loops.js
+++ loops.js
@@ -33,33 +33,89 @@
 
 // Another research work.
 
-let names = ['Cathy','Emmy','Mercy','Barbra','Julie'];
+// let names = ['Cathy','Emmy','Mercy','Barbra','Julie'];
 
-// Do while loop
+// // Do while loop
+// let index = 0;
+// do {
+//     if(index==3)
+//     {
+//      console.log('this is barbra:' + names[3]);
+//      document.write('this is my name:' + names[3]);
+//     }
+//     index++;
+// } while(index<names.length);
+
+// //For for loop
+// for(let name of names){
+//     if(name=='barbra')
+//     {
+//     console.log('this is my name:' + name);
+//     document.write('this is my name:' + name)
+// }
+// }
+
+// //for each loop
+// names.forEach(function(name, index){
+//     if(name=='barbra'){
+//         console.log('this is my name:' + name);
+//         document.write('this is my name:' + name)
+//     }
+// });
+
+
+// final task consider this.
+//ARRAY TO STORE THE NAMES 
+let names = ['Cathy', 'Emmy', 'Mercy', 'Barbra', 'Julie'];
+
+
+//DO WHILE LOOP
+
+//Declaring and intializing variable index
 let index = 0;
+
 do {
-    if(index==3)
+
+   //Checking for index value at every iteration
+    if (index == 3)
     {
-     console.log('this is barbra:' + names[3]);
-     document.write('this is my name:' + names[3]);
+
+    //Printing the result if the index value equals
+   console.log('This is my name : ' + names[3]);
+
     }
+  
+  //Incrementing the index value by 1
     index++;
-} while(index<names.length);
 
-//For for loop
-for(let name of names){
-    if(name=='barbra')
+//Checking the condition before the next iteration 
+} while ( index < names.length );
+
+
+//FOR OF LOOP
+for (let name of names) 
+{
+    //Checking for name Barbra at every iteration
+    if (name == 'Barbra')
     {
-    console.log('this is my name:' + name);
-    document.write('this is my name:' + name)
-}
+	
+  //Printing the result if the name equals 
+	console.log('This is my name : ' + name)
+   
+ }
 }
 
-//for each loop
-names.forEach(function(name, index){
-    if(name=='barbra'){
-        console.log('this is my name:' + name);
-        document.write('this is my name:' + name)
+
+//FOR EACH LOOP
+names.forEach(function (name, index) 
+{
+	//Checking for name Barbra at every iteration
+	if (name == 'Barbra')
+    {
+
+	//Printing the result if the name equals 
+	console.log('This is my name : ' + name)
+
     }
-});
 
+});
