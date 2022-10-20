be797b256cbdcaae8b721963ca9e64c8d4bdf536
imuria20bbailey <barbraoluka20@gmail.com>
2022-10-20 23:11:57 +0300

    another work

--- loops.js
+++ loops.js
@@ -5,28 +5,61 @@
 
 // Do...while...loop
 
-var names = ["barbra this is my name","joan","john"];
+// var names = ["barbra this is my name","joan","john"];
 
-do {
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-  names++
+// do {
+//     console.log(names[0]);
+//     console.log(names[1]);
+//     console.log(names[2]);
+//   names++
    
-} while(names[0])
+// } while(names[0])
 
-// For...of...loop
+// // For...of...loop
 
-var names = ["barbra","joy","josh","brian"];
+// var names = ["barbra","joy","josh","brian"];
 
-for (n of names){
-  console.log(n)
+// for (n of names){
+//   console.log(n)
+// }
+
+// // For...each...loop
+// let array = ["barbra","jimmy","dante","grace"];
+
+// array.forEach((item, index) => {
+
+//     console.log(item);
+// });
+
+// Another research work.
+
+let names = ['Cathy','Emmy','Mercy','Barbra','Julie'];
+
+// Do while loop
+let index = 0;
+do {
+    if(index==3)
+    {
+     console.log('this is barbra:' + names[3]);
+     document.write('this is my name:' + names[3]);
+    }
+    index++;
+} while(index<names.length);
+
+//For for loop
+for(let name of names){
+    if(name=='barbra')
+    {
+    console.log('this is my name:' + name);
+    document.write('this is my name:' + name)
+}
 }
 
-// For...each...loop
-let array = ["barbra","jimmy","dante","grace"];
-
-array.forEach((item, index) => {
-
-    console.log(item);
+//for each loop
+names.forEach(function(name, index){
+    if(name=='barbra'){
+        console.log('this is my name:' + name);
+        document.write('this is my name:' + name)
+    }
 });
+
